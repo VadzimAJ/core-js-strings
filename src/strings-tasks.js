0580a09a13fb46dfcd79fc5547bec8ad2bb25adf
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2023-11-16 16:08:22 +0300

    feat: complite 'concatinatin' and 'first character' lessons

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -78,8 +78,9 @@ function concatenateStrings(value1, value2) {
  *   getFirstChar('cat') => 'c'
  *   getFirstChar('') => ''
  */
-function getFirstChar(/* value */) {
-  throw new Error('Not implemented');
+function getFirstChar(value) {
+  const startLetter = String(value.charAt(0));
+  return startLetter;
 }
 
 /**
