ad7709b832a481807b23840d90bf07bfd4cf898f
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2023-11-16 15:44:15 +0300

    feat: complite 1 and 2 lessons

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -19,8 +19,8 @@
  *   getStringLength(null) => 0
  *   getStringLength(undefined) => 0
  */
-function getStringLength(/* value */) {
-  throw new Error('Not implemented');
+function getStringLength(value) {
+  return typeof value === 'string' ? value.length : 0;
 }
 
 /**
@@ -37,8 +37,17 @@ function getStringLength(/* value */) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+
+function isString(value) {
+  if (typeof value === 'string') {
+    return true;
+  }
+
+  if (value && value.constructor === String) {
+    return true;
+  }
+
+  return false;
 }
 
 /**
